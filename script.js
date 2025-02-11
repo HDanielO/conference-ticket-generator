61e51955ce35d249305f1bab0d2fb18453998285
HDanielO <hameeddnl@gmail.com>
2025-02-11 04:32:28 +0100

    implemented email verification functionality

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 "use strict";
+const emailInputElm = document.querySelector("#emailInput");
+const emailValidStatusElm = document.querySelector(".emailValidStatus");
 
+//Email verification functionality
+
+const handleEmailVerification = () => {
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  const emailInputValue = emailInputElm.value;
+
+  if (!emailRegex.test(emailInputValue)) {
+    emailValidStatusElm.classList.remove("displayNone");
+    emailInputElm.style.border = "2px solid var(--orange-500)";
+  } else {
+    emailValidStatusElm.classList.add("displayNone");
+    emailInputElm.style.border = "2px solid var(--neutral-500)";
+  }
+};
